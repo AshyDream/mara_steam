06c438b37708615d51d86fcd1e307fb47045713a
AshyDream <petro.mrtnk@hotmail.com>
2023-08-29 17:34:33 +0300

    added simple json reader for config file

---
+++ handlers/cfg.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Configuration struct {
+	Token string
+}
+
+func Cfg() Configuration {
+	file, _ := os.Open("config.json")
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	cfg := Configuration{}
+	err := decoder.Decode(&cfg)
+	if err != nil {
+		fmt.Println("Error cfg: ", err)
+	}
+	return cfg
+}
