2d844a4aa5e5cf46394fcae94c6cb39baa85a6a7
ashe <petro.mrtnk@hotmail.com>
2023-10-28 16:54:27 +0300

    Added file for connection and query DataBase

---
+++ db.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func InitDB() error {
+	dataSourceName := dbSource
+	var err error
+	db, err = sql.Open("mysql", dataSourceName)
+	if err != nil {
+		return err
+	}
+
+	// Встановлення параметрів з'єднання
+	db.SetMaxOpenConns(5)                  // Максимальна кількість відкритих з'єднань
+	db.SetMaxIdleConns(0)                  // Максимальна кількість неактивних з'єднань у пулі
+	db.SetConnMaxLifetime(3 * time.Minute) // Максимальний час життя з'єднання
+
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ShowDB() {
+	rows, err := db.Query("SHOW TABLES")
+	if err != nil {
+		fmt.Printf("Error getting Database structure: %v", err)
+		return
+	}
+
+	defer rows.Close()
+
+	var tableName string
+	for rows.Next() {
+		err := rows.Scan(&tableName)
+		if err != nil {
+			fmt.Printf("Error scanning row: %v", err)
+			return
+		}
+		fmt.Println("\033[34m", tableName)
+		ShowTable(tableName)
+	}
+}
+
+func ShowTable(name string) {
+	fmt.Println("\033[33m===========================================")
+	fmt.Println("\033[33mField  Type NULL Key DefaultVal Extra")
+	rows, err := db.Query("DESCRIBE " + name)
+	if err != nil {
+		fmt.Printf("Error describe table - %v : %v", name, err)
+		return
+	}
+
+	defer rows.Close()
+
+	var (
+		field      string
+		typ        string
+		null       string
+		key        string
+		defaultVal sql.NullString
+		extra      string
+	)
+
+	for rows.Next() {
+		err := rows.Scan(&field, &typ, &null, &key, &defaultVal, &extra)
+		if err != nil {
+			fmt.Printf("Error scanning row: %v", err)
+			return
+		}
+		fmt.Println(field, typ, null, key, defaultVal, extra)
+	}
+	fmt.Println("\033[33m===========================================\n")
+}
