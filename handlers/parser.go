e74aba3229b2e37a11099ca3b16f1c8938db35de
ashe <petro.mrtnk@hotmail.com>
2023-10-28 16:53:29 +0300

    rewrited parser. Added country currency option and map for storing information.

--- handlers/parser.go
+++ handlers/parser.go
@@ -2,14 +2,84 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/mymmrac/telego"
 	"log"
 	"net/http"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
 func Parser(url string, u *telego.Update, b *telego.Bot) {
+
+	doc := fetch(url)
+
+	gameTitle := doc.Find(".apphub_AppName").First().Text()
+	fmt.Println(gameTitle)
+
+	purchaseBox := doc.Find(".game_area_purchase").Find(".game_area_purchase_game")
+
+	discCheck := purchaseBox.First().Find(".game_purchase_discount").Text()
+
+	if discCheck == "" {
+		return
+	}
+
+	var gameInfo = map[string]map[string]string{
+		"UA": map[string]string{},
+		"US": map[string]string{},
+		"GE": map[string]string{},
+		"UK": map[string]string{},
+		"JP": map[string]string{},
+		"CA": map[string]string{},
+		"AU": map[string]string{},
+		"NZ": map[string]string{},
+		"NO": map[string]string{},
+		"CH": map[string]string{},
+		"TW": map[string]string{},
+		"AR": map[string]string{},
+		"BR": map[string]string{},
+		"ID": map[string]string{},
+		"KR": map[string]string{},
+		"MX": map[string]string{},
+		"PL": map[string]string{},
+		"EU": map[string]string{},
+	}
+
+	for key := range gameInfo {
+		nUrl := url + "?cc=" + key
+		fmt.Println(key)
+		doc := fetch(nUrl)
+		purchaseBox := doc.Find(".game_area_purchase").Find(".game_area_purchase_game")
+		info := gatherInfo(purchaseBox)
+		gameInfo[key]["pct"] = info[0]
+		gameInfo[key]["priceActl"] = info[1]
+		gameInfo[key]["priceDscnt"] = info[2]
+	}
+
+	fmt.Println(gameInfo)
+
+}
+
+func gatherInfo(doc *goquery.Selection) []string {
+	gameBox := doc.First()
+
+	//gameName := gameBox.Find("h1").Text()
+	//fmt.Println(gameName)
+
+	gamePrices := gameBox.Find(".discount_prices").Children()
+	gameDscntPct := gameBox.Find(".discount_pct").Text()
+	gameActlPrice := gamePrices.First().Text()
+	gameDscntPrice := gamePrices.Next().Text()
+
+	//for i, box := range purchaseBox {
+	//	fmt.Printf("\033[3%vm %v", i+1, box.Attr)
+	//}
+
+	info := []string{gameDscntPct, gameActlPrice, gameDscntPrice}
+	return info
+}
+
+func fetch(url string) *goquery.Document {
+
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("Error get response from %s: %s", url, err)
@@ -17,7 +87,7 @@ func Parser(url string, u *telego.Update, b *telego.Bot) {
 	defer response.Body.Close()
 
 	if response.StatusCode != 200 {
-		fmt.Printf("Error status code %v", response.StatusCode)
+		fmt.Printf("Error status code: %v", response.StatusCode)
 	}
 
 	doc, err := goquery.NewDocumentFromReader(response.Body)
@@ -25,39 +95,5 @@ func Parser(url string, u *telego.Update, b *telego.Bot) {
 		log.Fatal(err)
 	}
 
-	gameTitle := doc.Find(".apphub_AppName").First().Text()
-	fmt.Println(gameTitle)
-
-	firstPurchaseBlock := doc.Find(".game_area_purchase_game").First()
-	//titleCheck := checkPurchaseBlock(firstPurchaseBlock)
-	//fmt.Println(titleCheck)
-
-	buyBlock := firstPurchaseBlock.Find(".game_purchase_action").First()
-	discBlock := buyBlock.Find(".discount_block.game_purchase_discount").First()
-	fmt.Println(discBlock.Text())
-	//discCheck := discBlock.Find(".game_purchase_action_bg").First()
-	//if len(discCheck.Nodes) > 1 {
-	//	return
-	//}
-	discBlockText := discBlock.Text()
-	if discBlockText != "" {
-		pricePct := discBlock.Find(".discount_pct").Text()
-		oldPrice := discBlock.Find(".discount_original_price").Text()
-		newPrice := discBlock.Find(".discount_final_price").Text()
-		mText := gameTitle + ": \nDiscount: " + pricePct + "\nOld price: " + oldPrice + "\nNew price " + newPrice + "\n" + url
-		message := telego.SendMessageParams{
-			ChatID: telego.ChatID{ID: u.Message.Chat.ID, Username: u.Message.From.Username},
-			Text:   mText,
-		}
-		_, err := b.SendMessage(&message)
-		if err != nil {
-			fmt.Printf("\033[31mparser.go l52 can't send a message")
-		}
-		return
-	}
-	fmt.Printf("\033[31mNo discount for %s right now\033[0m", gameTitle)
+	return doc
 }
-
-//func checkPurchaseBlock(block *goquery.Selection) bool {
-//
-//}
