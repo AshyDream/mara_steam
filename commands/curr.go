929235f5a4528b4650f05f1e6899a7b1caa405fa
ashe <petro.mrtnk@hotmail.com>
2023-11-03 16:03:15 +0200

    Added some line of code to check if database queries working

--- commands/curr.go
+++ commands/curr.go
@@ -2,11 +2,17 @@ package commands
 
 import (
 	"github.com/mymmrac/telego"
+	"mara/dbQueries"
 	"mara/handlers"
 )
 
 func init() {
 	handlers.Register("curr", func(u *telego.Update, b *telego.Bot) {
+		chatId := u.Message.Chat.ID
+		if !dbQueries.IsUser(int(chatId)) {
+			dbQueries.AddNewUser(u, "UA")
+			return
+		}
 
 	})
 }
