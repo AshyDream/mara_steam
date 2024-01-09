3b26c377176814f05a1a22c094a0e0011a67a96e
ashe <petro.mrtnk@hotmail.com>
2024-01-09 14:20:14 +0200

    Writed command with button-menu

--- commands/curr.go
+++ commands/curr.go
@@ -2,17 +2,34 @@ package commands
 
 import (
 	"github.com/mymmrac/telego"
-	"mara/dbQueries"
 	"mara/handlers"
 )
 
+var (
+	buttons = [][]telego.InlineKeyboardButton{
+		{{Text: "\U0001F1FA\U0001F1E6", CallbackData: "UA"}},
+		{{Text: "\U0001F1FA\U0001F1F8", CallbackData: "US"}},
+		{{Text: "\U0001F1E9\U0001F1EA", CallbackData: "GE"}},
+		{{Text: "\U0001F1F5\U0001F1F1", CallbackData: "PL"}},
+	}
+)
+
 func init() {
+	//newB := []telego.InlineKeyboardButton{
+	//	{Text: "dsa", CallbackData: "dsa"},
+	//}
+	//buttons := append(buttons, newB)
 	handlers.Register("curr", func(u *telego.Update, b *telego.Bot) {
-		chatId := u.Message.Chat.ID
-		if !dbQueries.IsUser(int(chatId)) {
-			dbQueries.AddNewUser(u, "UA")
-			return
+		keyboardMarkup := telego.InlineKeyboardMarkup{
+			InlineKeyboard: buttons,
 		}
+
+		message := telego.SendMessageParams{
+			Text:        "Here u go",
+			ChatID:      telego.ChatID{ID: u.Message.Chat.ID},
+			ReplyMarkup: &keyboardMarkup,
+		}
+		b.SendMessage(&message)
 
 	})
 }
