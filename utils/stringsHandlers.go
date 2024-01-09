cb5f0865c9ba985579a63ac2318f733881fa6fb6
ashe <petro.mrtnk@hotmail.com>
2024-01-09 14:27:38 +0200

    Added handler for id's

---
+++ utils/stringsHandlers.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+func TrimFirstRune(s string) string {
+	_, i := utf8.DecodeRuneInString(s)
+	return s[i:]
+}
+
+func IdTrimer(s string) int {
+	pathSegments := strings.Split(s, "/")
+
+	appIndex := -1
+	for i, segment := range pathSegments {
+		if segment == "app" && i+1 < len(pathSegments) {
+			appIndex = i + 1
+			break
+		}
+	}
+
+	if appIndex == -1 || appIndex >= len(pathSegments) {
+		fmt.Println("Invalid URL structure")
+		return 0
+	}
+
+	var result strings.Builder
+
+	for _, char := range s {
+		if char >= '0' && char <= '9' {
+			result.WriteRune(char)
+		}
+	}
+
+	s = result.String()
+
+	id, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println("Error converting ID to integer:", err)
+		return 0
+	}
+
+	return id
+}
+
+func UrlBuilder(gameId int) string {
+	urlExmpl := "https://store.steampowered.com/app/"
+	return urlExmpl + string(gameId)
+}
+
+func StringToInt(str string) int {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return i
+}
