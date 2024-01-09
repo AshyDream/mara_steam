b0bc4464864fd25fe33e9b1b9cf85e863b6151d4
ashe <petro.mrtnk@hotmail.com>
2024-01-09 14:26:53 +0200

    Added cookies for mature content, added go routines so parsing will be faster. And rewrited fetch func so it can use cookies

--- handlers/parser.go
+++ handlers/parser.go
@@ -5,6 +5,8 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/mymmrac/telego"
 	"log"
+	"mara/dbQueries"
+	"mara/utils"
 	"net/http"
 )
 
@@ -12,58 +14,87 @@ func Parser(url string, u *telego.Update, b *telego.Bot) {
 
 	url += "?l=english"
 
-	doc := fetch(url)
+	cookies := []*http.Cookie{
+		{Name: "wants_mature_content", Value: "1"},
+		{Name: "lastagecheckage", Value: "1-0-1983"},
+		{Name: "birthtime", Value: "407541601"},
+	}
+	doc := fetch(url, cookies)
 
 	gameTitle := doc.Find(".apphub_AppName").First().Text()
 	fmt.Println(gameTitle)
 
 	purchaseBox := doc.Find(".game_area_purchase").Find(".game_area_purchase_game")
 
-	discCheck := purchaseBox.First().Find(".game_purchase_discount").Text()
+	var discCheck string
+	if doc.Find(".demo_above_purchase").Text() != "" {
+		discCheck = purchaseBox.First().Next().Find(".game_purchase_discount").Text()
+	} else {
+		discCheck = purchaseBox.First().Find(".game_purchase_discount").Text()
+	}
 
-	if discCheck == "" {
+	id := utils.IdTrimer(url)
+
+	if dbQueries.OnSale(id) {
+		if discCheck == "" {
+			dbQueries.OnSaleChanger(1, id)
+			return
+		}
 		return
+	} else {
+		if discCheck == "" {
+			return
+		} else {
+			dbQueries.OnSaleChanger(0, id)
+		}
 	}
 
 	var gameInfo = map[string]map[string]string{
 		"UA": {},
 		"US": {},
 		"GE": {},
-		"UK": {},
-		"JP": {},
-		"CA": {},
-		"AU": {},
-		"NZ": {},
-		"NO": {},
-		"CH": {},
-		"TW": {},
-		"AR": {},
-		"BR": {},
-		"ID": {},
-		"KR": {},
-		"MX": {},
+		//"UK": {},
+		//"JP": {},
+		//"CA": {},
+		//"AU": {},
+		//"NZ": {},
+		//"NO": {},
+		//"CH": {},
+		//"TW": {},
+		////"AR": {},
+		////"BR": {},
+		//"ID": {},
+		//"KR": {},
+		//"MX": {},
 		"PL": {},
-		"EU": {},
+		//"EU": {},
 	}
 
 	for key := range gameInfo {
 		nUrl := url + "&cc=" + key
 		fmt.Println(key)
-		doc := fetch(nUrl)
-		purchaseBox := doc.Find(".game_area_purchase").Find(".game_area_purchase_game")
-		info := gatherInfo(purchaseBox)
-		gameInfo[key]["pct"] = info[0]
-		gameInfo[key]["priceActl"] = info[1]
-		gameInfo[key]["priceDscnt"] = info[2]
+		doc := fetch(nUrl, cookies)
+		go func() {
+			info := gatherInfo(doc)
+			gameInfo[key]["pct"] = info[0]
+			gameInfo[key]["priceActl"] = info[1]
+			gameInfo[key]["priceDscnt"] = info[2]
+		}()
 	}
 
 	fmt.Println(gameInfo)
 
 }
 
-func gatherInfo(doc *goquery.Selection) []string {
-	gameBox := doc.First()
+func gatherInfo(doc *goquery.Document) []string {
+	purchaseBox := doc.Find(".game_area_purchase").Find(".game_area_purchase_game")
 
+	var gameBox goquery.Selection
+	if doc.Find(".demo_above_purchase").Text() != "" {
+		gameBox = *purchaseBox.First().Next().Find(".game_purchase_discount")
+	} else {
+		gameBox = *purchaseBox.First().Find(".game_purchase_discount")
+	}
 	//gameName := gameBox.Find("h1").Text()
 	//fmt.Println(gameName)
 
@@ -80,21 +111,32 @@ func gatherInfo(doc *goquery.Selection) []string {
 	return info
 }
 
-func fetch(url string) *goquery.Document {
+func fetch(url string, cookies []*http.Cookie) *goquery.Document {
 
-	response, err := http.Get(url)
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Printf("Error get response from %s: %s", url, err)
+		log.Printf("Error creating HTTP request: %v", err)
+	}
+
+	for _, cookie := range cookies {
+		req.AddCookie(cookie)
+	}
+
+	response, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error HTTP fetch: %v", err)
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != 200 {
-		fmt.Printf("Error status code: %v", response.StatusCode)
+		log.Printf("Wrong Status CODE: %d", response.StatusCode)
 	}
 
 	doc, err := goquery.NewDocumentFromReader(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Error parsing page: %v", err)
 	}
 
 	return doc
